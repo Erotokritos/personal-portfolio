517e5a7d673a6b01fdd57432a788b86563a08d9c
Erotokritos <erotokritossapounas@gmail.com>
2023-06-22 14:35:45 +0300

    Added Buttons on the project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,33 @@
-import { Col } from "react-bootstrap";
+import { Col,Row} from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl, projectUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, projectUrl, projectGitHub }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <a href={projectUrl} target="_blank">
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt="sample"/>
         <div className="proj-txtx">
           <h5>{title}</h5>
           <span>{description}</span>
+          <div className="proj-btn">
+            <Row>
+            {
+              projectUrl!=="#"?(
+                <>
+                <a href={projectUrl} target="_blank" className="btn btn-secondary btn-lg active" role="button">Demo</a>
+                </>):
+                <></>}
+            {
+              projectGitHub!=="#"?(
+                <>
+                <a href={projectGitHub} target="_blank" className="btn btn-secondary btn-lg active" role="button">GitHub</a>
+                </>
+              ):
+              <></>
+            }
+          </Row>
+          </div>
         </div>
       </div>
-      </a>
     </Col>
   )
 }
